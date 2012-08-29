30bdcf686cb6b296b225362c67880e4da1646dd2
Fernando Rodríguez Sela <frsela@tid.es>
2012-08-29 20:09:50 +0200

    Bug 746074 - Allow enforcing network-usage policies on 'web apps', r=philikon

--- modules/libpref/src/init/all.js
+++ modules/libpref/src/init/all.js
@@ -3648,6 +3648,9 @@ pref("dom.mozAlarms.enabled", false);
 // WebSettings
 pref("dom.mozSettings.enabled", false);
 
+// NetworkPolicies
+pref("dom.mozNetworkPolicies.enabled", false);
+
 // enable JS dump() function.
 pref("browser.dom.window.dump.enabled", false);
 
